6c8102d4ae90ddcc94d816621ba506d4579437f8
Sebastian <sebaacevedo.d@gmail.com>
2022-11-03 17:55:47 -0300

    ejercicios

---
+++ ejercicios/numbers.py
@@ -0,0 +1,22 @@
+
+
+#Asigna un valor entero 2001 a la variable space_odyssey y muestra su valor.
+
+#Descubre el tipo del literal 'Good night & Good luck'.
+
+#Identifica el tipo del literal True.
+
+#Asigna la expresión 10 * 3.0 a la variable result y muestra su tipo.
+
+
+space_odyssey = 2001
+print(space_odyssey)
+
+night_gl = 'Good night & Good luck'
+print(type(night_gl))
+
+True
+print(type(True))
+
+result = 10 * 3.0
+print(type(result))
